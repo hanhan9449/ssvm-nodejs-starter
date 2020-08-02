b79678eaddfe9600ccb0ad91c14f4fe63c16173a
Han Han <haannn@qq.com>
2020-08-02 17:15:07 +0800

    add CORS

--- node/app.js
+++ node/app.js
@@ -5,7 +5,14 @@ const fileUpload = require("express-fileupload");
 const hostname = "0.0.0.0";
 const port = 3000;
 const app = express();
-
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.use(express.static("public"));
 app.use(fileUpload());
 
